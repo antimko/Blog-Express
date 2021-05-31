ccb23087e3b9654377fb9fbc9d60d5d5f0e9428a
Andrejus Timko <timcostudios@gmail.com>
2021-05-31 09:49:23 +0300

    ejs installed so on

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const app = express()
+const path = require("path")
  
 const PORT = 3000;
+app.set("view engine", "ejs");
 
-
+app.set("views", "src/views");
+//home page
 app.get('/', function (req, res) {
-  res.send('Hello World')
+//   res.sendFile(path.join(__dirname, "pages", "index.html"));
+  res.render('index');
 });
+
+
+
+// about page
+app.get('/about', function (req, res) {
+    // res.sendFile(path.join(__dirname, "pages", "about.html"));
+    res.render('about')
+      });
+
+
+  
+
+  app.get('/blogs', function (req, res) {
+    // res.sendFile(path.join(__dirname, "pages", "blogs.html"));
+    res.render('blogs')
+  });
+
+  const staticPath = path.join(__dirname, "static")
+  app.use(express.static(staticPath));
+
 app.use((req, res)=>res.status(404).send("OOPs! The page was't found!"))
  
 app.listen(PORT);
+// console.log(staticPath);
